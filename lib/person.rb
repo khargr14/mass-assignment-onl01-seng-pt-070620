16898bfa7b5be6a410144c1d6ec8fb576319993d
Keesha Hargrove <kharg14@gmail.com>
2020-08-08 16:13:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,14 @@
 class Person
   #your code here
- Mass Assignment Lab
-  
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width 
+
+  # Your initialize method should use iteration and the .send method to mass 
+  # assign the value of each key/value pair to its associated key (i.e. method).
+  #  Refer to the previous reading to help you solve this one.
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
