2bac448a3bd2ebb4d6f0069e44502b2e28530631
Keesha Hargrove <kharg14@gmail.com>
2020-08-08 16:12:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,5 @@
 class Person
   #your code here
-  
-  avi_attributes =  { name: "Avi",
-      birthday :   "01/29/1984",
-      hair_color:  "brown",
-      eye_color:  "brown",
-      height:  "short",
-      weight:  "good",
-      handed:  "lefty",
-      complexion:  "decent",
-    t_shirt_size:  "medium",
-      wrist_size: "small",
-      glove_size: "normal",
-      pant_length:  "32",
-      pant_width:  "32"}
-  
- avi = Person.new (avi_attributes)
+ Mass Assignment Lab
   
 end
